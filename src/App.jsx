776aaf40efc600eda59b20a3ac43912b7254c766
hpweisberg <hpweisberg@gmail.com>
2023-07-12 17:23:51 -0700

    begin creation of edit page

--- src/App.jsx
+++ src/App.jsx
@@ -32,6 +32,7 @@ import '../src/styles/index.css'
 import BottomNavBar from './components/BottomNavBar/BottomNavBar'
 import FriendList from './pages/FriendList/FriendList'
 import NotificationsPage from './pages/NotificationsPage/NotificationsPage'
+import EditProfile from './pages/EditProfile/EditProfile'
 
 function App() {
   const navigate = useNavigate()
@@ -267,6 +268,15 @@ function App() {
             element={
               <ProtectedRoute user={user}>
                 <NotificationsPage profile={profile} user={user} />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/edit-profile"
+            element={
+              <ProtectedRoute user={user}>
+                <EditProfile profile={profile} user={user} />
               </ProtectedRoute>
             }
           />
