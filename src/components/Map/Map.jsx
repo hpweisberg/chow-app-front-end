0eef07fecb88ec5eaedcfd98d524e6d23115c747
hpweisberg <hpweisberg@gmail.com>
2023-07-14 13:25:08 -0700

    display card on click, click on map to remove card. click on card to nav to details

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,7 +1,10 @@
+import { useState } from 'react';
 // Map.js
 
 import { GoogleMap, Marker, LoadScript } from "@react-google-maps/api";
 import { useCallback, useMemo, useRef } from "react";
+
+import PostCardNew from "../PostCardNew";
 
 const Map = ({posts}) => {
   const mapRef = useRef();
@@ -11,12 +14,23 @@ const Map = ({posts}) => {
     clickableIcons: false,
 
   }), []);
+
+  const [selectedPost, setSelectedPost] = useState(null);
   
   const onLoad = useCallback(map => (mapRef.current = map), []);
 
-  const handleMarkerClick = (posts) => {
-    console.log(posts)
+  const handleMarkerClick = (post) => {
+    setSelectedPost(post)
+    console.log(post)
   } 
+
+  const handleCardClick = (post) => {
+    console.log('navigate to post: ', post._id)
+  }
+
+  const handleCardClose = () => {
+    setSelectedPost(null)
+  }
 
   const myLatlng = { lat: 33.983841, lng: -118.451424 };
 
@@ -28,13 +42,27 @@ const Map = ({posts}) => {
       <div className="w-full h-[550px]">
         {/* <GoogleMap zoom={8} center={{ lat: -34.397, lng: 150.644 }} /> */}
         {/* <LoadScript > */}
-        <GoogleMap className="w-full h-full" mapContainerClassName="w-full h-full" center={center} zoom={12} options={options} onLoad={onLoad}>
+        <GoogleMap 
+        className="w-full h-full" 
+        mapContainerClassName="w-full h-full" 
+        center={center} 
+        zoom={12} 
+        options={options} 
+        onLoad={onLoad}
+        onClick={handleCardClose}
+        >
           You can add markers or other map components here
           {posts.map((post) => {
               const { restaurant } = post;
               if (restaurant && restaurant.lat && restaurant.lng) {
                 const position = { lat: restaurant.lat, lng: restaurant.lng };
-                return <Marker key={post._id} position={position} />;
+                console.log('position: ', post)
+                return (
+                <Marker 
+                key={post._id} 
+                position={position} 
+                onClick={() => handleMarkerClick(post)}
+                />);
               }
               return null;
             })}
@@ -50,6 +78,13 @@ const Map = ({posts}) => {
         </GoogleMap>
         {/* </LoadScript> */}
       </div>
+      {selectedPost && (
+        <PostCardNew
+          post={selectedPost}
+          onClose={() => setSelectedPost(null)}
+          onCardClick={handleCardClick}
+        />
+      )}
     </>
   );
 };
