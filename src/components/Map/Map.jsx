a9273db088e1fbffab099a1d302f1284e21b662c
hpweisberg <hpweisberg@gmail.com>
2023-07-14 13:46:23 -0700

    markers show on load fix

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,12 +1,10 @@
-import { useState } from 'react';
-// Map.js
-
-import { GoogleMap, Marker, LoadScript } from "@react-google-maps/api";
+import { useState, useEffect } from 'react';
+import { GoogleMap, Marker } from "@react-google-maps/api";
 import { useCallback, useMemo, useRef } from "react";
 
 import PostCardNew from "../PostCardNew";
 
-const Map = ({posts}) => {
+const Map = ({ posts }) => {
   const mapRef = useRef();
   const center = useMemo(() => ({ lat: 33.983841, lng: -118.451424 }), []);
   const options = useMemo(() => ({
@@ -16,13 +14,14 @@ const Map = ({posts}) => {
   }), []);
 
   const [selectedPost, setSelectedPost] = useState(null);
-  
+  const [markers, setMarkers] = useState([]);
+
   const onLoad = useCallback(map => (mapRef.current = map), []);
 
   const handleMarkerClick = (post) => {
     setSelectedPost(post)
     console.log(post)
-  } 
+  }
 
   const handleCardClick = (post) => {
     console.log('navigate to post: ', post._id)
@@ -32,51 +31,43 @@ const Map = ({posts}) => {
     setSelectedPost(null)
   }
 
-  const myLatlng = { lat: 33.983841, lng: -118.451424 };
-
   console.log('postion: ', 'lat: ', posts.restaurant?.lat, 'lng: ', posts.restaurant?.lng)
+
+  useEffect(() => {
+    if (posts.length > 0) {
+      const newMarkers = posts.reduce((markerElements, post) => {
+        const { restaurant } = post;
+        if (restaurant && restaurant.lat && restaurant.lng) {
+          const position = { lat: restaurant.lat, lng: restaurant.lng };
+          markerElements.push(
+            <Marker
+              key={post._id}
+              position={position}
+              onClick={() => handleMarkerClick(post)}
+            />
+          );
+        }
+        return markerElements;
+      }, []);
+      setMarkers(newMarkers);
+    }
+  }, [posts]);
 
   return (
     <>
       <h1>Map</h1>
       <div className="w-full h-[550px]">
-        {/* <GoogleMap zoom={8} center={{ lat: -34.397, lng: 150.644 }} /> */}
-        {/* <LoadScript > */}
-        <GoogleMap 
-        className="w-full h-full" 
-        mapContainerClassName="w-full h-full" 
-        center={center} 
-        zoom={12} 
-        options={options} 
-        onLoad={onLoad}
-        onClick={handleCardClose}
+        <GoogleMap
+          className="w-full h-full"
+          mapContainerClassName="w-full h-full"
+          center={center}
+          zoom={12}
+          options={options}
+          onLoad={onLoad}
+          onClick={handleCardClose}
         >
-          You can add markers or other map components here
-          {posts.map((post) => {
-              const { restaurant } = post;
-              if (restaurant && restaurant.lat && restaurant.lng) {
-                const position = { lat: restaurant.lat, lng: restaurant.lng };
-                console.log('position: ', post)
-                return (
-                <Marker 
-                key={post._id} 
-                position={position} 
-                onClick={() => handleMarkerClick(post)}
-                />);
-              }
-              return null;
-            })}
-          {/* <Marker position={myLatlng} /> */}
-          {/* /* <Marker position={{ lat: -34.397, lng: 150.644 }} /> */} */
-          {/* {posts.map((post) => ( */}
-            {/* <Marker
-              // key={post._id}
-              position={myLatlng}
-              // onClick={() => handleMarkerClick(post)}
-          /> */}
-          {/* ))} */}
+          {markers}
         </GoogleMap>
-        {/* </LoadScript> */}
       </div>
       {selectedPost && (
         <PostCardNew
