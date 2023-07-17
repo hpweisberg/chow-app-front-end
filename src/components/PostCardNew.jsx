7502708e9f472875d2fbe15885325cc36a6a4468
hpweisberg <hpweisberg@gmail.com>
2023-07-16 21:22:54 -0700

    add padding to the last post card in feed and profile

--- src/components/PostCardNew.jsx
+++ src/components/PostCardNew.jsx
@@ -1,11 +1,11 @@
 import { NavLink } from "react-router-dom";
 
-const PostCardNew = ({ post }) => {
+const PostCardNew = ({ post, isLast }) => {
   console.log('post: ', post)
 
   return (
     <NavLink to={`/posts/${post._id}`}>
-      <div className="mb-1 overflow-hidden bg-white border rounded-lg shadow-lg max-h-48">
+      <div className={`mb-1 overflow-hidden bg-white border rounded-lg shadow-lg max-h-48 ${isLast ? 'mb-20' : ''}`}>
         <div className="flex">
           <div className="w-1/4">
             <img src={post.photo} alt="Restaurant" className="object-cover h-full " />
